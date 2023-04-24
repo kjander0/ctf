cd22f9d8069c3565a545f1254d15a08034ad4c57
Kieren Anderson <kjander0@gmail.com>
2023-04-24 12:53:36 +1000

    Fix flag pickup/drops

--- entity/flag.go
+++ entity/flag.go
@@ -19,25 +19,48 @@ func NewFlag(pos mymath.Vec) Flag {
 func UpdateFlags(world *World) {
 	for i := range world.FlagList {
 		flag := &world.FlagList[i]
-		for j := range world.PlayerList {
-			player := &world.PlayerList[j]
 
-			if flag.Held {
-				if player.FlagIndex == i {
-					if player.State != PlayerStateAlive {
-						flag.Held = false
-						player.FlagIndex = -1
-					} else {
-						flag.Pos = player.Acked.Pos
-					}
+		if flag.Held {
+			flag.Held = false
+			for j := range world.PlayerList {
+				player := &world.PlayerList[j]
+				if player.FlagIndex != i {
+					continue
 				}
-			} else {
-				if player.State == PlayerStateAlive && player.Acked.Pos.DistanceTo(flag.Pos) < float64(conf.Shared.TileSize) {
-					flag.Held = true
-					player.FlagIndex = i
+				if player.State != PlayerStateAlive {
+					player.FlagIndex = -1
+					continue
+				}
+				flag.Held = true
+				flag.Pos = player.Acked.Pos
+			}
+		}
+
+		if !flag.Held {
+			var closestPlayer *Player = nil
+			var closestDist float64
+			for j := range world.PlayerList {
+				player := &world.PlayerList[j]
+				if player.State != PlayerStateAlive {
+					continue
+				}
+
+				dist := player.Acked.Pos.DistanceTo(flag.Pos)
+				if dist > float64(2*conf.Shared.TileSize) {
+					continue
+				}
+				if closestPlayer == nil || dist < closestDist {
+					closestPlayer = player
+					closestDist = dist
 				}
 			}
 
+			if closestPlayer != nil {
+				closestPlayer.FlagIndex = i
+				flag.Held = true
+				flag.Pos = closestPlayer.Acked.Pos
+			}
 		}
+
 	}
 }
